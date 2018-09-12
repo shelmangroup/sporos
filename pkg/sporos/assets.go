a5f1f2ef099b533ad526c93c026ef6707465fb1e
Lars Sjöström <lars@radicore.se>
2018-09-12 10:34:49 +0200

    create etcd tls secrets

--- pkg/sporos/assets.go
+++ pkg/sporos/assets.go
@@ -1,21 +1,22 @@
 package sporos
 
 import (
-	"fmt"
 	"net"
 	"net/url"
-	"strings"
 
 	"github.com/kubernetes-incubator/bootkube/pkg/asset"
 	"github.com/kubernetes-incubator/bootkube/pkg/tlsutil"
 	api "github.com/shelmangroup/sporos/pkg/apis/sporos/v1alpha1"
 
+	"github.com/operator-framework/operator-sdk/pkg/sdk"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/client-go/kubernetes/scheme"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	// "k8s.io/client-go/kubernetes/scheme"
 )
 
 func prepareAssets(cr *api.Sporos) error {
-	decode := scheme.Codecs.UniversalDeserializer().Decode
+	// decode := scheme.Codecs.UniversalDeserializer().Decode
 
 	apiserver, _ := url.Parse(cr.Spec.ApiServerUrl)
 	_, podCIDR, _ := net.ParseCIDR(cr.Spec.PodCIDR)
@@ -36,19 +37,92 @@ func prepareAssets(cr *api.Sporos) error {
 	if err != nil {
 		return err
 	}
-	for _, a := range assets {
-		if strings.HasPrefix(a.Name, "manifests") {
-			obj, _, err := decode(a.Data, nil, nil)
-			if err != nil {
-				return fmt.Errorf("Error while decoding YAML object. Err was: %s", err)
-			}
-			switch o := obj.(type) {
-			case *corev1.Secret:
-				fmt.Println(o.Data)
-			default:
-				continue
-			}
-		}
+
+	err = createEtcdTLSsecrets(cr, assets)
+	if err != nil {
+		return err
 	}
+	return nil
+}
+
+func createEtcdTLSsecrets(cr *api.Sporos, a asset.Assets) error {
+	//Create server cert secret
+	serverKey, _ := a.Get(asset.AssetPathEtcdServerKey)
+	serverCert, _ := a.Get(asset.AssetPathEtcdServerCert)
+	serverCa, _ := a.Get(asset.AssetPathEtcdServerCA)
+	serverSecret := &corev1.Secret{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Secret",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      EtcdServerTLSSecretName(cr.Name),
+			Namespace: cr.Namespace,
+			Labels:    LabelsForSporos(cr.Name),
+		},
+		Data: map[string][]byte{
+			"server.key":    serverKey.Data,
+			"server.crt":    serverCert.Data,
+			"server-ca.crt": serverCa.Data,
+		},
+	}
+	addOwnerRefToObject(serverSecret, asOwner(cr))
+	err := sdk.Create(serverSecret)
+	if err != nil && !apierrors.IsAlreadyExists(err) {
+		return err
+	}
+
+	//Create client cert secret
+	clientKey, _ := a.Get(asset.AssetPathEtcdClientKey)
+	clientCert, _ := a.Get(asset.AssetPathEtcdClientCert)
+	clientCa, _ := a.Get(asset.AssetPathEtcdClientCA)
+	clientSecret := &corev1.Secret{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Secret",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      EtcdClientTLSSecretName(cr.Name),
+			Namespace: cr.Namespace,
+			Labels:    LabelsForSporos(cr.Name),
+		},
+		Data: map[string][]byte{
+			"etcd-client.key":    clientKey.Data,
+			"etcd-client.crt":    clientCert.Data,
+			"etcd-client-ca.crt": clientCa.Data,
+		},
+	}
+	addOwnerRefToObject(clientSecret, asOwner(cr))
+	err = sdk.Create(clientSecret)
+	if err != nil && !apierrors.IsAlreadyExists(err) {
+		return err
+	}
+
+	//Create peer cert secret
+	peerKey, _ := a.Get(asset.AssetPathEtcdPeerKey)
+	peerCert, _ := a.Get(asset.AssetPathEtcdPeerCert)
+	peerCa, _ := a.Get(asset.AssetPathEtcdPeerCA)
+	peerSecret := &corev1.Secret{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Secret",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      EtcdPeerTLSSecretName(cr.Name),
+			Namespace: cr.Namespace,
+			Labels:    LabelsForSporos(cr.Name),
+		},
+		Data: map[string][]byte{
+			"peer.key":    peerKey.Data,
+			"peer.crt":    peerCert.Data,
+			"peer-ca.crt": peerCa.Data,
+		},
+	}
+	addOwnerRefToObject(peerSecret, asOwner(cr))
+	err = sdk.Create(peerSecret)
+	if err != nil && !apierrors.IsAlreadyExists(err) {
+		return err
+	}
+
 	return nil
 }
