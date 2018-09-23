efe4a3899432782bef2870bedc7a3fd7f4832349
Lars Sjöström <lars@radicore.se>
2018-09-23 14:27:27 +0200

    added admin kubeconfig

--- pkg/sporos/assets.go
+++ pkg/sporos/assets.go
@@ -11,6 +11,8 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/tools/clientcmd"
+	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
 )
 
 type Asset struct {
@@ -49,12 +51,12 @@ func prepareAssets(cr *api.Sporos) error {
 		return err
 	}
 
-	apiServers := []string{"localhost", cr.Status.ApiServerIP}
+	apiServers := []string{"localhost", fmt.Sprintf("%s-kube-api-server.%s.svc", cr.Name, cr.Namespace), cr.Status.ApiServerIP}
 	controlplaneAssets, err := newTLSAssets(caCert, caKey, apiServers)
 	if err != nil {
 		return err
 	}
-	err = createControlplaneSecrets(cr, controlplaneAssets)
+	err = createControlplaneSecrets(cr, append(controlplaneAssets, etcdAssets...))
 	if err != nil {
 		return err
 	}
@@ -149,6 +151,10 @@ func createControlplaneSecrets(cr *api.Sporos, a Assets) error {
 	caKey, _ := a.Get("ca.key")
 	apiserverKey, _ := a.Get("apiserver.key")
 	apiserverCert, _ := a.Get("apiserver.crt")
+	etcdClientKey, _ := a.Get("etcd-client.crt")
+	etcdClientCert, _ := a.Get("etcd-client.key")
+	etcdClientCa, _ := a.Get("etcd-client-ca.crt")
+	serviceAccountPub, _ := a.Get("service-account.pub")
 	apiserverSecret := &corev1.Secret{
 		TypeMeta: metav1.TypeMeta{
 			Kind:       "Secret",
@@ -160,14 +166,51 @@ func createControlplaneSecrets(cr *api.Sporos, a Assets) error {
 			Labels:    LabelsForSporos(cr.Name),
 		},
 		Data: map[string][]byte{
-			"apiserver.key": apiserverKey.Data,
-			"apiserver.crt": apiserverCert.Data,
-			"ca.crt":        caCert.Data,
-			"ca.key":        caKey.Data,
+			"apiserver.key":       apiserverKey.Data,
+			"apiserver.crt":       apiserverCert.Data,
+			"ca.crt":              caCert.Data,
+			"ca.key":              caKey.Data,
+			"etcd-client.key":     etcdClientKey.Data,
+			"etcd-client.crt":     etcdClientCert.Data,
+			"etcd-client-ca.crt":  etcdClientCa.Data,
+			"service-account.pub": serviceAccountPub.Data,
 		},
 	}
 	addOwnerRefToObject(apiserverSecret, asOwner(cr))
 	err := sdk.Create(apiserverSecret)
+	if err != nil && !apierrors.IsAlreadyExists(err) {
+		return err
+	}
+
+	adminKey, _ := a.Get("admin.key")
+	adminCert, _ := a.Get("admin.crt")
+	adminConfig := clientcmdapi.NewConfig()
+	adminConfig.Clusters["local"].Server = fmt.Sprintf("https://%s-kube-api-server.%s.svc", cr.Name, cr.Namespace)
+	adminConfig.Clusters["local"].CertificateAuthorityData = caCert.Data
+	adminConfig.AuthInfos["admin"].ClientCertificateData = adminCert.Data
+	adminConfig.AuthInfos["admin"].ClientKeyData = adminKey.Data
+	adminConfig.Contexts["context"].AuthInfo = "admin"
+	adminConfig.Contexts["context"].Cluster = "local"
+	adminConfigData, err := clientcmd.Write(*adminConfig)
+	if err != nil {
+		return err
+	}
+	adminSecret := &corev1.Secret{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Secret",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      fmt.Sprintf("%s-admin-kubeconfig", cr.Name),
+			Namespace: cr.Namespace,
+			Labels:    LabelsForSporos(cr.Name),
+		},
+		Data: map[string][]byte{
+			"kubeconfig": adminConfigData,
+		},
+	}
+	addOwnerRefToObject(adminSecret, asOwner(cr))
+	err = sdk.Create(adminSecret)
 	if err != nil && !apierrors.IsAlreadyExists(err) {
 		return err
 	}
